ddc96a0ff7083cccac297dae68ff3d2b9a5025cc
ZenChris18 <121231422+ZenChris18@users.noreply.github.com>
2024-06-07 02:16:01 +0800

    Made redirect if people tried to access the profile

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-def init_app(app):
-    db.init_app(app)
+    def __repr__(self):
+        return f'<User {self.username}>'
